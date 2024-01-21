28a009b0d96bb775995ba9ebbb0a87061d4b5887
Nathan Bass <119013498+Nathan-A-B@users.noreply.github.com>
2024-01-21 16:30:34 -0600

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,9 @@
+from mac_vendor_lookup import MacLookup
+from tkinter import scrolledtext, messagebox
+from tkinter import *
+import tkinter as tk
+import bluetooth
+from scapy.all import Ether, ARP
+from scapy.all import *
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
